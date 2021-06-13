19a35767414e0395b24f99a3242d563217a6ed50
Meaghan Crowley <meaghan@sungage.com>
2021-06-12 17:25:51 -0700

    transportation edits

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -37,13 +37,13 @@ class HomePage extends React.Component {
                             </Col>
                             <Col md={{ span: 19 }} xs={{ span: 24}} style={{ paddingLeft: '26px'}}>
                                 {date == 7 && (
-                                    <div style={{width: '100%', display: 'flex', alignItems: 'center', marginBottom: '10px', padding: '15px 0px', border: '1px solid #86b59f', borderRadius: '5px', cursor: 'pointer'}}>
+                                    <a href="/transportation" style={{width: '100%', display: 'flex', alignItems: 'center', marginBottom: '10px', padding: '15px 0px', border: '1px solid #86b59f', borderRadius: '5px', cursor: 'pointer'}}>
 
-                                        <a href="/transportation" style={{textAlign: 'center', width: '100%', fontFamily: 'Montserrat', color: '#86b59f', display: 'flex', alignItems: 'center', justifyContent: 'center', '&:hover': {opacity: 0.5}}}>
+                                        <div style={{textAlign: 'center', width: '100%', fontFamily: 'Montserrat', color: '#86b59f', display: 'flex', alignItems: 'center', justifyContent: 'center', '&:hover': {opacity: 0.5}}}>
                                             <div style={{marginLeft: '15px', marginRight: '15px'}}><CarOutlined twoToneColor="#86b59f" style={{fontSize: 30}}/></div>
-                                            Transportation will be provided between events, click here for details
-                                        </a>
-                                    </div>
+                                            Click here to view the shuttle schedule
+                                        </div>
+                                    </a>
                                 )}
                                 {eventData[date] && eventData[date].map( event => (
                                     <EventInfo 
--- src/Components/Pages/Transportation.js
+++ src/Components/Pages/Transportation.js
@@ -22,18 +22,31 @@ const Transportation = () =>  {
     return(
         <div style={style.page}>
             <h1 style={{ paddingBottom: '25px' }}>Wedding Day Transportation</h1>
-            <p style={{textAlign: 'center'}}>Saturday is spread out across a few different venues and we want to make sure it's as easy as possible for folks to get around without driving. See below for details on provided transportation</p>
+            <p style={{textAlign: 'center'}}>Saturday's events are spread out across a few different venues and we want to make sure it's as easy as possible for folks to get around without driving. 
+            <br/>See below for details on provided transportation.</p>
             <Card style={style.card}>
                 <h4>2:30 - Charter Hotel to Gates Amphitheater</h4>
                 <p>A shuttle will be waiting for guests in front of the hotel</p>
                 <h4>3:30 - Gates Amphitheater to Charter Hotel</h4>
-                <p>Guests can return to the hotel for the not-so-catholic gap if they wish</p>
+                <p>
+                    Guests can return to the hotel for the "not-so-Catholic" gap if they wish. 
+                    <br/>Otherwise we recommend exploring the scultpure park or having a drink (or few) next door at <a href="https://pub70.com/" target="_blank">Pub 70 </a>
+                </p>
                 <h4>5:30 - Charter Hotel to Reception</h4>
                 <p>A shuttle will be waiting for guests in front of the hotel</p>
                 <h4>5:45 - Pub 70 to Reception</h4>
-                <p>The shuttle will go from the hotel to Pub 70 and head to Almquist Family Winery from there</p>
+                <p>The shuttle will stop at Pub 70 after picking up guest from the hotel on the way to Almquist Family Winery</p>
                 <h4>Post Reception - Lyft Code</h4>
-                <p>Guests can use our Lyft Code for $20 off. You can enter the code MEAGHANANDIAN21 in the Rewards section of the app before boking your ride. If you don't have Lyft <a href="https://www.lyft.com/i/MEAGHANANDIAN21" style={{textDecoration: 'underline'}}>you can download it here and the code will automatically be applied</a></p>
+                <p>
+                    <i>There will be no shuttle service after the reception.</i> 
+                    <br/>
+                    But we have arranged a special discount Lyft Code for $20 off any ride on Saturday between any of the venues. 
+                    <br/>
+                    You can enter the code <strong>MEAGHANANDIAN21</strong> in the Rewards section of the app before boking your ride. 
+                    <br/>
+                    If you don't have Lyft <a href="https://www.lyft.com/i/MEAGHANANDIAN21" style={{textDecoration: 'underline'}}>you can 
+                    download it here</a> and the code will automatically be applied.
+                </p>
             </Card>
             
         </div>
